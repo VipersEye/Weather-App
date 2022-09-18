672c66c5e9bea2bfba9cf8dca8f77416a91c653c
Nikolay <nyk190@mail.ru>
2022-09-19 00:30:20 +0300

    Refactoring: AppWeather class
    AppWeather.js
    
    Add
    updateWeather
    updateCurrentWeather
    getWeather
    requestWeather
    generateFetchURL functions
    
    Add Promise to class constructor, which will waiting for allowing or
    denying to geo location from user and when call updateWeather func.
    
    updateWeather func responsible for all weather data in app and
    calls updateCurrentWeather func automatically every 3h.
    
    updateCurrentWeather func calls getWeather func and updating all
    weather parameters in main section of app.
    
    getWeather func calls requestWeather func and reduce data to object.
    
    requestWeather func calls generateFetchURL func and fetch data about
    weather from API.
    
    generateFetchURL func generates url for fetch in requestWeather func.
    
    call stack:
    updateWeather -> updateCurrentWeather -> getWeather -> requestWeather
     -> generateFetchURL
    
    main.css
    Also add new .weather__param-forecast_same css class, which add if
    weather parameter wont change anytime soon.
    
    index.html
    Add id's for closest forecast in main section.

--- src/modules/AppWeather.js
+++ src/modules/AppWeather.js
@@ -1,45 +1,128 @@
 export default class AppWeather {
+
     constructor() {
-        console.log(this.currentWeather);
+        this.position = null;
+        this.city = null;
+        new Promise( (resolve, reject) => {
+            navigator.geolocation.getCurrentPosition( (position) => {
+                this.position = position;
+                resolve();
+            }, (error) => {
+                if (error.code === 1) {
+                    alert('Geolocation denied, please choose your city');
+                    this.city = prompt();
+                    resolve();
+                } else {
+                    reject(error);
+                }
+            } );
+        } ).then( () => {
+            this.updateWeather();
+        } ).catch( (error) => {
+            console.log(`unknown error ${error.code}: ${error.message}`);
+        } );        
+    }
+
+    updateWeather() {
         this.updateCurrentWeather();
+        
+        setTimeout(() => {
+            this.updateWeather();
+        }, 3 * 3600 * 1000);
     }
 
     async updateCurrentWeather() {
-        const requestWeather = async (position = false) => {
-            let request = new URL('https://api.openweathermap.org/data/2.5/weather');
-            
-            if (position) {
-                request.searchParams.set('lat', position.coords.latitude.toFixed(2));
-                request.searchParams.set('lon', position.coords.longitude.toFixed(2));
-            } else {
-                let city = prompt();
-                request.searchParams.set('q', city);
-            }
-            request.searchParams.set('appid', '949e7c85a5e24668e40350ebc260b5ad');
-            request.searchParams.set('units', 'metric');
-            
 
-            try {
-                let response = await fetch(request);
-                let weather = await response.json();
-                console.log(weather.name);
-                let {main:{temp:temperature, pressure, humidity}, wind:{speed:wind} } = weather;
-                
-                document.querySelector('#current-temperature').textContent = `${temperature.toFixed(0)}°`;
-                document.querySelector('#current-pressure').textContent = `${pressure} hpa`;
-                document.querySelector('#current-humidity').textContent = `${humidity}%`;
-                document.querySelector('#current-wind').textContent = `${wind} m/s`;
-            } catch(error) {
-                alert('wrong city try again');
-                requestWeather();
-            }
-        };
+        let weather = await this.getWeather();
 
-        const errorHandler = (error) => {
-            alert('You denied permission to your location, print name of city');
-            requestWeather();
-        };
+        for (let param in weather.difference) {
+            let weatherParameterCurrent = document.querySelector(`#current-${param}`);
+            weatherParameterCurrent.textContent = `${weather.current[param]}${weather.units[param]}`;
 
-        navigator.geolocation.getCurrentPosition( requestWeather, errorHandler );
+            let weatherParameterForecast = document.querySelector(`#forecast-${param}`);
+            weatherParameterForecast.textContent = `${weather.difference[param].value}${weather.units[param]}`;
+            weatherParameterForecast.classList.remove('weather__param-forecast_inc', 'weather__param-forecast_dec', 'weather__param-forecast_same');
+            weatherParameterForecast.classList.add(`weather__param-forecast_${weather.difference[param].change}`);
+        }
     }
+
+    async getWeather() {
+        const extractWeatherData = (data) => {
+            let {main: {temp: temperature, pressure, humidity}, wind: {speed: wind}} = data;
+            temperature = +temperature.toFixed(0);
+            wind = +wind.toFixed(0);
+            return {temperature, pressure, humidity, wind};
+        };
+        
+        let {weather: currentWeatherData, forecast: forecastWeatherData} = await this.requestWeather();
+
+        let weather = {
+            units: {
+                temperature: '°',
+                pressure: ' hpa',
+                humidity: '%',
+                wind: ' m/s'
+            },
+            current: extractWeatherData(currentWeatherData),
+            forecast: forecastWeatherData.list,
+            closest: extractWeatherData(forecastWeatherData.list[1]),
+            difference: {}
+        };
+
+        for (let param in weather.current) {
+            weather.difference[param] = {
+                value: Math.abs(weather.current[param] - weather.closest[param]).toFixed(0),
+                change: weather.current[param] > weather.closest[param] ? 'dec' : weather.current[param] < weather.closest[param] ? 'dec' : 'same' ,
+            };
+        }
+
+        return weather;
+
+    }
+
+    async requestWeather() {
+        try {
+            let urlWeather = this.generateFetchURL('weather');
+            let responseWeather = await fetch(urlWeather);
+            let weather = await responseWeather.json();
+
+            console.log(weather);
+            if (weather.message === 'city not found') throw new Error(weather.message);
+
+            let urlForecast = this.generateFetchURL('forecast');
+            let responseForecast = await fetch(urlForecast);
+            let forecast = await responseForecast.json();
+
+            return {weather, forecast};
+
+        } catch (error) {
+
+            console.log(error);
+            if (error.message === 'city not found') {
+                alert(`${this.city} not found, try again`);
+                this.city = prompt();
+                this.updateWeather();
+            } else if (error.message === 'Failed to fetch') {
+                alert('Error: check your internet connection and try again');
+                await new Promise( (resolve) => setTimeout( () => resolve() , 60000) );
+                this.updateWeather();
+            } else {
+                alert('Something went wrong :(');
+            }
+
+        }
+    }
+
+    generateFetchURL = (type) => {
+        let url = new URL(`https://api.openweathermap.org/data/2.5/${type}`);           
+        if (this.position) {
+            url.searchParams.set('lat', this.position.coords.latitude.toFixed(2));
+            url.searchParams.set('lon', this.position.coords.longitude.toFixed(2));
+        } else {
+            url.searchParams.set('q', this.city);
+        }
+        url.searchParams.set('appid', '949e7c85a5e24668e40350ebc260b5ad');
+        url.searchParams.set('units', 'metric');
+        return url;
+    };
 }
