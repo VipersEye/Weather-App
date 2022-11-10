0773f73f1e3858b7c24d80a5e1c170e1686ca4a0
Nikolay <nyk190@mail.ru>
2022-11-10 15:48:21 +0300

    Add input for change app color theme
        index.html
    Add input range for change app color theme.
    
        main.css
    Add css variable for main color of app and styles for input range.
    
        App.js
    Replace mostly all event listeners to AppAnimations class constructor.
    Add saving and settings app color theme.
    
        AppAnimations.js
    Add event listeners for cite animations in constructor, also add
    event listener for input range, which change css variable with main
    color.

--- src/modules/App.js
+++ src/modules/App.js
@@ -14,122 +14,6 @@ export default class App {
         this.player = playerModule;
         this.slider = sliderModule;
 
-        // listeners
-
-        const showNextSlide = (e) => {
-            let weatherParameter = e.currentTarget;
-            let slider = weatherParameter.querySelector('.weather__param-slider');
-            let currentSlideNumber = +slider.getAttribute('current');
-            let scroll = weatherParameter.classList.contains('weather__home-item_hover_down') ? 'down' : 'up' ;
-            let numberOfSlides = slider.children.length;
-            let nextSlide;
-        
-            if (scroll === 'down') {
-                if (currentSlideNumber + 1 > numberOfSlides - 1) return;
-                slider.setAttribute('current', currentSlideNumber + 1);
-                nextSlide = slider.children[currentSlideNumber + 1];
-            } else {
-                if (currentSlideNumber - 1 < 0) return; 
-                slider.setAttribute('current', currentSlideNumber - 1);
-                nextSlide = slider.children[currentSlideNumber - 1];
-            }
-
-            let paramIcon = nextSlide.querySelector('.weather__param-icon');
-            let paramName = nextSlide.querySelector('.weather__param-name');
-            let paramValue = nextSlide.querySelector('.weather__param-value');
-            let paramForecast = nextSlide.querySelector('.weather__param-forecast');
-            
-            paramIcon.classList.add(`slide-${scroll}`);
-            paramName.classList.add(`slide-${scroll}`, 'slide_delay_low');
-            paramValue.classList.add(`slide-${scroll}`, 'slide_delay_medium');
-            paramForecast.classList.add(`slide-${scroll}`, 'slide_delay_high');
-
-            let sliderHeight = slider.offsetHeight;
-            slider.scrollTop = scroll === 'down' ? slider.scrollTop + sliderHeight : slider.scrollTop - sliderHeight;
-
-            [paramIcon, paramName, paramValue, paramForecast].forEach((paramPart) => {
-                const removeAnimationClass = function remover(e) {
-                    paramPart.classList.remove(`slide-${scroll}`);
-                    e.target.removeEventListener('animationend', remover );
-                };
-                paramPart.addEventListener('animationend', removeAnimationClass);
-            });
-        };
-
-        const addHighlighting = (e) => {
-            let weatherParameter = e.currentTarget;
-            let height = weatherParameter.offsetHeight;
-            let rect = weatherParameter.getBoundingClientRect();
-            let mousePosY = e.clientY - rect.top;
-            if (mousePosY <= height / 2) {
-                weatherParameter.classList.remove('weather__home-item_hover_down');
-                weatherParameter.classList.add('weather__home-item_hover_up');
-            } else {
-                weatherParameter.classList.remove('weather__home-item_hover_up');
-                weatherParameter.classList.add('weather__home-item_hover_down');
-            }
-        };
-
-        const removeHighlighting = (e) => {
-            let weatherParameter = e.currentTarget;
-            weatherParameter.classList.remove('weather__home-item_hover_down', 'weather__home-item_hover_up');
-        };
-
-        const toggleContent = () => {
-            let navBtnActiveId = document.querySelector('.nav__btn_active').id; 
-            let currentActiveContent = document.querySelector('.content_show');
-            let nextShowContent = document.querySelector(`.weather__${navBtnActiveId}`);
-
-            currentActiveContent.classList.remove('content_show');
-            currentActiveContent.classList.add('content_hidden');
-
-            nextShowContent.classList.add('content_show');
-            nextShowContent.classList.remove('content_hidden');
-
-            blockTogglerOff.classList.add('toggler_off');
-        };
-
-        const toggleContentEnd = () => {
-            blockTogglerOn.classList.remove('toggler_on');
-            blockTogglerOff.classList.remove('toggler_off');
-        };
-
-        const toggleContentStart = (e) => {
-            if (e.currentTarget.classList.contains('nav__btn_active')) return;
-
-            let navBtn = e.currentTarget;
-            let currentActiveBtn = [...navBtns].find((btn) => btn.classList.contains('nav__btn_active'));
-            currentActiveBtn.classList.remove('nav__btn_active');
-            navBtn.classList.add('nav__btn_active');
-
-            let mainContent = document.querySelector('.main');
-            let mainContentWidth = mainContent.offsetWidth;
-            let mainContentHeight = mainContent.offsetHeight;
-            let togglerRadius = `${Math.ceil((mainContentWidth ** 2 + mainContentHeight ** 2) ** 0.5)}px`;
-            document.documentElement.style.setProperty('--toggler-radius', togglerRadius);
-
-            document.querySelector('#first-toggler').classList.add('toggler_on');
-        };
-
-   
-        let weatherParamSliders = document.querySelectorAll('.weather__home-item');
-        weatherParamSliders.forEach( (weatherParamSlider) => {
-            weatherParamSlider.addEventListener('mousemove', addHighlighting);
-            weatherParamSlider.addEventListener('click', showNextSlide);
-            weatherParamSlider.addEventListener('mouseleave', removeHighlighting);
-        });
-        
-
-        let blockTogglerOn = document.querySelector('#first-toggler');
-        let blockTogglerOff = document.querySelector('#second-toggler');
-        blockTogglerOn.addEventListener('animationend', toggleContent);
-        blockTogglerOff.addEventListener('animationend', toggleContentEnd);
-
-        let navBtns = document.querySelectorAll('.nav__btn');
-        navBtns.forEach((btn) => {
-            btn.addEventListener('click', toggleContentStart);
-        });
-
         let applySettingsBtn = document.querySelector('.settings__btn_apply');
         applySettingsBtn.addEventListener('click', this.applySettings.bind(this));
 
@@ -178,7 +62,8 @@ export default class App {
                 username,
                 units: 'metric',
                 animations: false,
-                volume: 0.5
+                theme: 252,
+                volume: 0.5,
             };
             localStorage.setItem('settings', JSON.stringify(defaultSettings));
         }
@@ -188,6 +73,8 @@ export default class App {
         document.querySelector('#input-username').value = settings.username;
         document.querySelector(`.settings__input_radio[value="${settings.units}"]`).checked = true;
         document.querySelector(`.settings__input_radio[value="${settings.animations ? 'on' : 'off'}"]`).checked = true;
+        document.querySelector('#input-theme').value = settings.theme;
+        document.documentElement.style.setProperty('--main-clr', settings.theme);
         document.querySelector('#input-volume').value = settings.volume;
         document.documentElement.style.setProperty('--range-saturation', `${settings.volume * 100}%`);
         document.querySelector('#btn-volume use').setAttribute('xlink:href',`#volume-icon-${settings.volume > 0.66 ? 'high' : settings.volume < 0.67 && settings.volume > 0.33 ? 'medium' : settings.volume < 0.34 && settings.volume > 0 ? 'low' : 'mute'}`);
@@ -215,13 +102,15 @@ export default class App {
             let username = document.querySelector('#input-username').value;
             let units = document.querySelector('.settings__input_radio[name="temp-metric"]:checked').value;
             let animations = document.querySelector('.settings__input_radio[name="animations"]:checked').value === 'on';
+            let theme = +document.querySelector('#input-theme').value;
             let volume = +document.querySelector('#input-volume').value;
             
             let settings = {
                 username,
                 units,
                 animations,
-                volume
+                theme,
+                volume,
             };
             localStorage.setItem('settings', JSON.stringify(settings));
         };
--- src/modules/AppAnimations.js
+++ src/modules/AppAnimations.js
@@ -1,5 +1,127 @@
 export default class AppAnimations {
-    constructor() {}
+    constructor() {
+        const showNextSlide = (e) => {
+            let weatherParameter = e.currentTarget;
+            let slider = weatherParameter.querySelector('.weather__param-slider');
+            let currentSlideNumber = +slider.getAttribute('current');
+            let scroll = weatherParameter.classList.contains('weather__home-item_hover_down') ? 'down' : 'up' ;
+            let numberOfSlides = slider.children.length;
+            let nextSlide;
+        
+            if (scroll === 'down') {
+                if (currentSlideNumber + 1 > numberOfSlides - 1) return;
+                slider.setAttribute('current', currentSlideNumber + 1);
+                nextSlide = slider.children[currentSlideNumber + 1];
+            } else {
+                if (currentSlideNumber - 1 < 0) return; 
+                slider.setAttribute('current', currentSlideNumber - 1);
+                nextSlide = slider.children[currentSlideNumber - 1];
+            }
+
+            let paramIcon = nextSlide.querySelector('.weather__param-icon');
+            let paramName = nextSlide.querySelector('.weather__param-name');
+            let paramValue = nextSlide.querySelector('.weather__param-value');
+            let paramForecast = nextSlide.querySelector('.weather__param-forecast');
+            
+            paramIcon.classList.add(`slide-${scroll}`);
+            paramName.classList.add(`slide-${scroll}`, 'slide_delay_low');
+            paramValue.classList.add(`slide-${scroll}`, 'slide_delay_medium');
+            paramForecast.classList.add(`slide-${scroll}`, 'slide_delay_high');
+
+            let sliderHeight = slider.offsetHeight;
+            slider.scrollTop = scroll === 'down' ? slider.scrollTop + sliderHeight : slider.scrollTop - sliderHeight;
+
+            [paramIcon, paramName, paramValue, paramForecast].forEach((paramPart) => {
+                const removeAnimationClass = function remover(e) {
+                    paramPart.classList.remove(`slide-${scroll}`);
+                    e.target.removeEventListener('animationend', remover );
+                };
+                paramPart.addEventListener('animationend', removeAnimationClass);
+            });
+        };
+
+        const addHighlighting = (e) => {
+            let weatherParameter = e.currentTarget;
+            let height = weatherParameter.offsetHeight;
+            let rect = weatherParameter.getBoundingClientRect();
+            let mousePosY = e.clientY - rect.top;
+            if (mousePosY <= height / 2) {
+                weatherParameter.classList.remove('weather__home-item_hover_down');
+                weatherParameter.classList.add('weather__home-item_hover_up');
+            } else {
+                weatherParameter.classList.remove('weather__home-item_hover_up');
+                weatherParameter.classList.add('weather__home-item_hover_down');
+            }
+        };
+
+        const removeHighlighting = (e) => {
+            let weatherParameter = e.currentTarget;
+            weatherParameter.classList.remove('weather__home-item_hover_down', 'weather__home-item_hover_up');
+        };
+
+        const toggleContent = () => {
+            let navBtnActiveId = document.querySelector('.nav__btn_active').id; 
+            let currentActiveContent = document.querySelector('.content_show');
+            let nextShowContent = document.querySelector(`.weather__${navBtnActiveId}`);
+
+            currentActiveContent.classList.remove('content_show');
+            currentActiveContent.classList.add('content_hidden');
+
+            nextShowContent.classList.add('content_show');
+            nextShowContent.classList.remove('content_hidden');
+
+            blockTogglerOff.classList.add('toggler_off');
+        };
+
+        const toggleContentEnd = () => {
+            blockTogglerOn.classList.remove('toggler_on');
+            blockTogglerOff.classList.remove('toggler_off');
+        };
+
+        const toggleContentStart = (e) => {
+            if (e.currentTarget.classList.contains('nav__btn_active')) return;
+
+            let navBtn = e.currentTarget;
+            let currentActiveBtn = [...navBtns].find((btn) => btn.classList.contains('nav__btn_active'));
+            currentActiveBtn.classList.remove('nav__btn_active');
+            navBtn.classList.add('nav__btn_active');
+
+            let mainContent = document.querySelector('.main');
+            let mainContentWidth = mainContent.offsetWidth;
+            let mainContentHeight = mainContent.offsetHeight;
+            let togglerRadius = `${Math.ceil((mainContentWidth ** 2 + mainContentHeight ** 2) ** 0.5)}px`;
+            document.documentElement.style.setProperty('--toggler-radius', togglerRadius);
+
+            document.querySelector('#first-toggler').classList.add('toggler_on');
+        };
+
+        const changeAppTheme = (e) => {
+            let newColor = e.target.value;
+            document.documentElement.style.setProperty('--main-clr', newColor);
+        };
+
+   
+        let weatherParamSliders = document.querySelectorAll('.weather__home-item');
+        weatherParamSliders.forEach( (weatherParamSlider) => {
+            weatherParamSlider.addEventListener('mousemove', addHighlighting);
+            weatherParamSlider.addEventListener('click', showNextSlide);
+            weatherParamSlider.addEventListener('mouseleave', removeHighlighting);
+        });
+        
+
+        let blockTogglerOn = document.querySelector('#first-toggler');
+        let blockTogglerOff = document.querySelector('#second-toggler');
+        blockTogglerOn.addEventListener('animationend', toggleContent);
+        blockTogglerOff.addEventListener('animationend', toggleContentEnd);
+
+        let navBtns = document.querySelectorAll('.nav__btn');
+        navBtns.forEach((btn) => {
+            btn.addEventListener('click', toggleContentStart);
+        });
+
+        let themeInputRange = document.querySelector('#input-theme');
+        themeInputRange.addEventListener('input', changeAppTheme);
+    }
 
     start() {
         this.setAnimations();
